647f9083bbd6f80628c34530f3cdd19cf72dc351
江锦华 <jjh6032@17u.cn>
2013-08-26 09:55:37 +0800

    initial

---
+++ Test2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test2
+{
+    /// <summary>
+    /// 测试
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pp = "edward";
+        }
+    }
+}
